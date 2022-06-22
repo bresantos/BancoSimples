0276726964e37169e320a45a1779211312646e64
bresantos <88750289+bresantos@users.noreply.github.com>
2022-06-22 16:29:24 -0300

    Add files via upload

---
+++ Banco.java
@@ -0,0 +1,15 @@
+package desafio;
+
+public class Banco {
+	
+	public String nome;
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Cliente.java
@@ -0,0 +1,24 @@
+package desafio;
+
+public class Cliente {
+	
+	public String nome;
+	
+		
+	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+}
